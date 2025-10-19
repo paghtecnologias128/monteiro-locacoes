ff0c42b1e7abf507514b62a2f53270f0a2f21a3e
Paulo - Damine <paulo.h.r.correa@gmail.com>
2025-10-19 13:11:33 -0400

    tranlate data

--- src/data/products.js
+++ src/data/products.js
@@ -1,45 +1,45 @@
-// Importe as imagens PNG
-import barracaBolinha from '../assets/ball_tent.png';
-import cadeiras from '../assets/chairs.png';
-import comboFesta from '../assets/party_combo.png';
-import pinturaFacil from '../assets/face_painting.png';
-import piscinaBolinha from '../assets/ball_pit.png';
-import pulaPula from '../assets/bouncy_castle.png';
+// Import PNG images
+import ballTent from '../assets/ball_tent.png';
+import chairs from '../assets/chairs.png';
+import partyCombo from '../assets/party_combo.png';
+import facePainting from '../assets/face_painting.png';
+import ballPit from '../assets/ball_pit.png';
+import bouncyCastle from '../assets/bouncy_castle.png';
 
 export const cardsData = [
   {
     id: 1,
-    image: piscinaBolinha,
+    image: ballPit,
     title: 'Piscina de Bolinhas',
     alt: 'Piscina de Bolinhas colorida com escorregador.',
   },
   {
     id: 2,
-    image: pulaPula,
+    image: bouncyCastle,
     title: 'Pula-Pula',
     alt: 'Pula-pula inflável com redes de proteção.',
   },
   {
     id: 3,
-    image: barracaBolinha,
+    image: ballTent,
     title: 'Barra de Bolinhas',
     alt: 'Barraca de bolinhas inflável com tema de circo.',
   },
   {
     id: 4,
-    image: cadeiras,
+    image: chairs,
     title: 'Mesas + Cadeiras',
     alt: 'Conjunto de mesas e cadeiras de plástico brancas.',
   },
   {
     id: 5,
-    image: pinturaFacil,
+    image: facePainting,
     title: 'Pintura Facial',
     alt: 'Criança com o rosto pintado de borboleta.',
   },
   {
     id: 6,
-    image: comboFesta,
+    image: partyCombo,
     title: 'Combo Festa',
     alt: 'Imagem com vários brinquedos de festa juntos.',
   },
